0971afa4802ca9392d92cff573f4f628474b4fc9
Miechael888 <miechaeltakumansang@gmail.com>
2022-07-06 21:43:53 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Task 1
+
+// var bilangan = prompt("input bilangan");
+// var pembagi = prompt("input pembagi");
+// var kuadrat = prompt("input kuadrat");
+
+// console.log(bilangan / pembagi);
+// console.log(Math.pow(bilangan, kuadrat));
+
+// Task 3
+
+// let now = New Date();
+// now = now.toDateString();
+// console.log(now);
+
+// let kemarin = new Date(now);
+// kemarin.setDate(kemarin.getDate() - 1);
+// console.log(kemarin.toDateString());
+
+// let besok = new Date(now);
+// besok.setDate(besok.getDate() + 1);
+// console.log(besok.toDateString());
+
+// Task 5
+
+var kalimat = prompt("input kalimat");
+console.log(kalimat.length);
